2fe024ee632a3f6039b48efd65a11664d0af4678
Nickolai Belakovski <nbelakovski@gmail.com>
2025-09-29 17:10:16 -0400

    Move test_constraint_type to pycommon and modify it
    test_constraint_type is mostly testing the code we have been moving so
    far. It needed a couple superficial modiciations and one modification to
    test criterion for the "unsupported_type" test, since we are now
    outputting the type of the constraint that we don't recognize.
    
    Ref: #252

--- pycommon/tests/test_constraint_types.py
+++ pycommon/tests/test_constraint_types.py
@@ -1,8 +1,14 @@
-from prima import minimize, LinearConstraint as LC, NonlinearConstraint as NLC
+import pytest
+try:
+    from prima import minimize, LinearConstraint as LC, NonlinearConstraint as NLC
+except ModuleNotFoundError:
+    try:
+        from pyprima import minimize, LinearConstraint as LC, NonlinearConstraint as NLC
+    except ModuleNotFoundError:
+        pytest.fail(reason="Could not find either prima or pyprima libraries")
 from scipy.optimize import LinearConstraint as ScipyLC, NonlinearConstraint as ScipyNLC
 import numpy as np
 from objective import fun
-import pytest
 
 def test_prima_lc_is_scipy_lc():
     assert LC is ScipyLC
@@ -14,7 +20,12 @@ def test_linear_constraint_object():
     res = minimize(fun, x0, constraints=lc)
     assert np.isclose(res.x[0], 4.5, atol=1e-6, rtol=1e-6)
     assert np.isclose(res.x[1], 3.5, atol=1e-6, rtol=1e-6)
-    assert np.isclose(res.fun, 0.5, atol=1e-6, rtol=1e-6)
+    if hasattr(res, 'fun'):
+        assert np.isclose(res.fun, 0.5, atol=1e-6, rtol=1e-6)
+    elif hasattr(res, 'f'):
+        assert np.isclose(res.f, 0.5, atol=1e-6, rtol=1e-6)
+    else:
+        raise Exception("Result has no attribute for function value")
 
 
 def test_linear_constraint_dict():
@@ -23,7 +34,12 @@ def test_linear_constraint_dict():
     res = minimize(fun, x0, constraints=lc)
     assert np.isclose(res.x[0], 4.5, atol=1e-6, rtol=1e-6)
     assert np.isclose(res.x[1], 3.5, atol=1e-6, rtol=1e-6)
-    assert np.isclose(res.fun, 0.5, atol=1e-6, rtol=1e-6)
+    if hasattr(res, 'fun'):
+        assert np.isclose(res.fun, 0.5, atol=1e-6, rtol=1e-6)
+    elif hasattr(res, 'f'):
+        assert np.isclose(res.f, 0.5, atol=1e-6, rtol=1e-6)
+    else:
+        raise Exception("Result has no attribute for function value")
     
 
 def test_prima_nlc_is_scipy_nlc():
@@ -36,7 +52,12 @@ def test_nonlinear_constraint_object():
     res = minimize(fun, x0, constraints=nlc)
     assert np.isclose(res.x[0], 5, atol=1e-6, rtol=1e-6)
     assert np.isclose(res.x[1], 5, atol=1e-6, rtol=1e-6)
-    assert np.isclose(res.fun, 1, atol=1e-6, rtol=1e-6)
+    if hasattr(res, 'fun'):
+        assert np.isclose(res.fun, 1, atol=1e-6, rtol=1e-6)
+    elif hasattr(res, 'f'):
+        assert np.isclose(res.f, 1, atol=1e-6, rtol=1e-6)
+    else:
+        raise Exception("Result has no attribute for function value")
 
 
 def test_nonlinear_constraint_dict():
@@ -45,7 +66,12 @@ def test_nonlinear_constraint_dict():
     res = minimize(fun, x0, constraints=nlc)
     assert np.isclose(res.x[0], 5, atol=1e-6, rtol=1e-6)
     assert np.isclose(res.x[1], 5, atol=1e-6, rtol=1e-6)
-    assert np.isclose(res.fun, 1, atol=1e-6, rtol=1e-6)
+    if hasattr(res, 'fun'):
+        assert np.isclose(res.fun, 1, atol=1e-6, rtol=1e-6)
+    elif hasattr(res, 'f'):
+        assert np.isclose(res.f, 1, atol=1e-6, rtol=1e-6)
+    else:
+        raise Exception("Result has no attribute for function value")
 
 
 def test_unsupported_type_raises_exception():
@@ -55,4 +81,4 @@ def test_unsupported_type_raises_exception():
     
     with pytest.raises(ValueError) as e_info:
         minimize(fun, x0, constraints=lc)
-    assert e_info.match('Constraint type not recognized')
+    assert e_info.match("Constraint type <class 'dict'> not recognized")
